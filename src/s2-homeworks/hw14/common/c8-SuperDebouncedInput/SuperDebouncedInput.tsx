aa85254527b00c26876b37acedd3e8fcc9c9c32c
maksimselchukou <selchukouwebdev1995@gmail.com>
2023-06-07 15:06:54 +0300

    feat:fix SuperDebouncedInput component,no layout

--- src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
+++ src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
@@ -33,11 +33,13 @@ const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
 
         if (onDebouncedChange) {
             // делает студент
-            const tmID = setTimeout(() => {
+            clearTimeout(timerId)
+            let tmID = window.setTimeout(() => {
                 onDebouncedChange(value)
             }, 1500)
+            setTimerId(tmID)
 
-            clearTimeout(tmID)
+
             // остановить предыдущий таймер
             // запустить новый на 1500ms, в котором вызовется функция
 
